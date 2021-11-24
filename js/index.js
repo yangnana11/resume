450e683cfa183cf1b6c2ed26bee33d47449a68ea
yangnana11 <yphan1988@gmail.com>
2021-11-23 19:24:51 -0500

    update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
 //$(function(){
 //    var cw = $('.profile').width();
 //    $('.profile').css({'height':cw+'px'});
-////    var ch = $('.head').height();
-////    $('.head-desp').css({'line-height': (ch/2)+'px'});
-////    $('.head-info').css({'line-height': (ch/6)+'px'});
+//    var ch = $('.head').height();
+//    $('.head-desp').css({'line-height': (ch/2)+'px'});
+//    $('.head-info').css({'line-height': (ch/6)+'px'});
 //});
-//$(function(){
-//    var cw = $('.profile').width();
-//    $('.profile').css({'height':cw+'px'});
-//})
